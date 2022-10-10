b09af5f8e42fcbbd276a6ab14f533a537c805120
jan-martinek <jan-martinek@users.noreply.github.com>
2022-10-10 16:09:10 +0000

    Deploying to docs from @ books-are-next/zak-navrat-krale-sumavy@9356f7d61fb528ef35ce2ca67740f721f175583c 🚀

--- epub2nb-output/content/about.md
+++ epub2nb-output/content/about.md
@@ -5,11 +5,10 @@ title: Tiráž
 **David Jan Žák    
 Návrat Krále Šumavy**  
 **Román o Josefu Hasilovi**  
-Návrh obálky Jiří Troskov  
 Redakce Markéta Teuchnerová  
 Vydala Městská knihovna v Praze  
 Mariánské nám. 1, 115 72 Praha 1  
-V MKP 2. elektronické vydání z 7. 10. 2022.
+V MKP 2. elektronické vydání z 10. 10. 2022.
 
 ***
 
--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-eb65efc';
+const CACHE = 'cache-9356f7d';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
